2a9e78d76992685102998d0a1948a0194f77bf37
ArjunD0702 <arjund0702@gmail.com>
2025-09-07 14:36:03 +0000

    Add solution for Find N Unique Numbers Sum up to Zero

---
+++ easy/find-n-unique-integers-sum-up-to-zero.c
@@ -0,0 +1,30 @@
+// Problem: 1304. Find N Unique Integers Sum up to Zero
+// Link: https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+// Time Complexity: 
+// Memory Complexity: 
+#include <stdlib.h>
+
+int *sumZero(int n, int *returnSize) {
+    int *arr = malloc(n * sizeof(int)); 
+    if (arr == NULL) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    int i = 0;
+
+    if ((n % 2) != 0){
+        arr[i++] = 0;
+    }
+    
+    for(int k = 1; i < n; k++) {
+        arr[i++] = k;
+        
+        if (i < n) {
+            arr[i++] = -k;
+        }
+    }
+
+    *returnSize = n;
+    return arr;
+}
