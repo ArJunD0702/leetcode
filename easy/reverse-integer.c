570c313df3bae23ce575cb2af443abc7c17de92b
ArjunD0702 <arjund0702@gmail.com>
2025-09-07 06:22:09 +0000

    Add solution for Reverse Integer

---
+++ easy/reverse-integer.c
@@ -0,0 +1,27 @@
+// Problem: 7. Reverse Integer
+// Link: https://leetcode.com/problems/reverse-integer/
+// Time Complexity: O(logn)
+// Memory Complexity: O(1)
+
+#include <stdio.h>
+#include <limits.h>
+
+int reverse(int x) {
+    int reversed = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        x /= 10;
+
+        if (reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && digit > 7)) {
+            return 0;
+        }
+        if (reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && digit < -8) {
+            return 0;
+        }
+
+        reversed = reversed * 10 + digit;
+    }
+
+    return reversed;
+}
