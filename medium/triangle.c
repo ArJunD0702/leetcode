0a3c619c14a7d2e593f43d3208c4ddcbe3ace24f
ArjunD0702 <arjund0702@gmail.com>
2025-09-11 10:57:08 +0000

    Add solution for Triangle

---
+++ medium/triangle.c
@@ -0,0 +1,21 @@
+int minimumTotal(int **triangle, int triangleSize, int *triangleColSize) {
+    if (triangleSize == 0) {
+        return 0;
+    }
+
+    for (int i = triangleSize - 2; i >= 0; i--) {
+        for (int j = 0; j < triangleColSize[i]; j++) {
+            int a = triangle[i+1][j];
+            int b = triangle[i+1][j+1];
+
+            if (a < b) {
+                triangle[i][j] += a;
+            }
+            else {
+                triangle[i][j] += b;
+            }
+        }
+    }
+
+    return triangle[0][0];
+}
