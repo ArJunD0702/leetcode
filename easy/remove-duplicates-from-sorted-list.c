10fa725aa2c7222bd6766abdf67555161a0ab317
ArjunD0702 <arjund0702@gmail.com>
2025-10-25 14:18:20 +0000

    Adding solution for a linked list problem

---
+++ easy/remove-duplicates-from-sorted-list.c
@@ -0,0 +1,29 @@
+// Problem: 83. Remove Duplicates from Sorted List
+// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+// Time Complexity: O(m + n) 
+// Memory Complexity: O(1) 
+ 
+
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode *cur = head;
+
+    while (cur && cur->next) {
+        if (cur->val == cur->next->val) {
+            struct ListNode *dup = cur->next;
+            cur->next = dup->next;
+            free(dup);
+        } 
+        else {
+            cur = cur->next;
+        }
+    }
+
+    return head;
+}
