46e7d0d1ad91c796a40a2ca661c3d1bb571df361
ArjunD0702 <arjund0702@gmail.com>
2025-10-31 07:27:38 +0000

    Add solution to problem 49

---
+++ medium/group-anagrams.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for (auto &word : strs) {
+            string key = word;
+            sort(key.begin(), key.end());
+            groups[key].push_back(word);
+        }
+
+        vector<vector<string>> result;
+        for (auto &group : groups) {
+            result.push_back(group.second);
+        }
+
+        return result;
+    }
+};
